567ab5eb003fc1adc181dfce36ed0ebcbfe7397b
kajol <kajolthapa194@gmail.com>
2026-08-31 20:39:30 +0545

    removed api test and added lint

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -8,20 +8,3 @@ def test_api_is_running():
     assert response.status_code == 200
 
 
-def test_add_income():
-    data = {
-        "user_id": 1,
-        "amount": 1000.00,
-        "source": "Salary",
-        "payment_method": "Bank Transfer",
-        "income_date": "2026-08-31",
-        "note": "CI test income"
-    }
-
-    response = requests.post(
-        f"{BASE_URL}/add-income",
-        json=data
-    )
-
-    assert response.status_code == 200
-    assert response.json()["message"] == "income added"
