623f3bd1b8abe0027222fad94f5355a5cff4f832
kajol <kajolthapa194@gmail.com>
2026-09-11 16:53:33 +0545

    fixed padding

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,14 +2,5 @@
 import '../pages/Dashboard.css';
 import { FaBell } from 'react-icons/fa';
 export default function Navbar(){
-    return(
-    //  <div className="dashboard-header">
-    //     <h1 className="dashboard-title">Dashboard</h1>
-        <div
-          className="header-right"
-          style={{ paddingBottom: "10px" }}
-        >
-        </div>
-    //   </div>
-    );
+    return null;
 }
